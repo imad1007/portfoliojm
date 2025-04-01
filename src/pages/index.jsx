3e4a18c952b6253d13ed4774982057d55735f0f6
imad <imadhamdouch2@gmail.com>
2025-04-01 13:40:22 +0000

    first commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,13 +13,13 @@ export default function Hero() {
               Bonjour 👋, je m'appelle
             </p>
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-zinc-950">
-              Youssef baghdadi
+            Djamil Salhi
             </h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-zinc-950">
               étudiant en 2ème année de BTS SIO
             </p>
             <a
-              href="/pdf/youssef.pdf"
+              href="/pdf/jamil.pdf"
               class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
             >
               Consultez mon CV
@@ -40,7 +40,7 @@ export default function Hero() {
           <div class=" lg:mt-0 lg:col-span-5 mt-4">
             <img
 
-              src="/images/youssef.jpeg"
+              src="/images/heroImage.png"
               alt="mockup"
               className="rounded-xl h-96  w-full m-auto"
             />
